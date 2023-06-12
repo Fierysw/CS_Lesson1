2552bd74c0b9ca145a708a19f724fbbc059d04b9
Ваше имя английскими буквами <fieryswv@yandex.ru>
2023-06-12 10:15:30 +0300

    Создание репозитория

---
+++ Task 4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число от 1 и больше: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 1;
+if (n < 1) {
+  Console.WriteLine("Упс, число меньше 1");
+} 
+if (n == 1) {
+  Console.WriteLine("У единицы нет чётных чисел");
+} 
+if (n > 1) {
+  Console.WriteLine("Чётные числа запрашиваемого числа " + n + ": ");
+  while (count <= n) {
+    if (count % 2==0) {
+      Console.Write(count + " / ");
+      }
+    count++;
+  }
+}
